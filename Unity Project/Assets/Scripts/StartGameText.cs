04e7cfd963b5fd8a8813ee9372cc573d176ee61a
SimonBoersma <0354326@student.rocvantwente.nl>
2024-09-09 14:19:24 +0200

    Change gefixed
    Ik heb FindFirstObjectOfType veranderd naar FindObjectOfType

--- Unity Project/Assets/Scripts/StartGameText.cs
+++ Unity Project/Assets/Scripts/StartGameText.cs
@@ -6,7 +6,7 @@ public class StartGameText : MonoBehaviour
     [SerializeField] GameObject playText;
     void Start()
     {
-        ballMovement = FindFirstObjectByType<BallMovement>();
+        ballMovement = FindObjectOfType<BallMovement>();
     }
 
     void Update()
